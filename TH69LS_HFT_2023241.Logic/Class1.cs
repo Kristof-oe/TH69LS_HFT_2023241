8febc350e15fb1e66d513c81bf568f6840c1e464
Kristof-oe <kristof.ay@stud.uni-obuda.hu>
2023-10-17 22:03:57 +0200

    Add project files.

---
+++ TH69LS_HFT_2023241.Logic/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TH69LS_HFT_2023241.Logic
+{
+    public class Class1
+    {
+    }
+}
---
+++ TH69LS_HFT_2023241.Repository/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TH69LS_HFT_2023241.Repository
+{
+    public class Class1
+    {
+    }
+}
