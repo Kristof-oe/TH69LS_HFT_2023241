816ef13e3ce73bdb9bacf98541eb66c1b6d47057
Kristof-oe <kris200036@gmail.com>
2023-11-17 22:23:59 +0100

    IRepository has been implemented in abstract Repository

--- TH69LS_HFT_2023241.Repository/Repository.cs
+++ TH69LS_HFT_2023241.Repository/Repository.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace TH69LS_HFT_2023241.Repository
 {
-    public abstract class Repository
+    public abstract class Repository<T> : IRepository<T> where T : class
     {
+        public void Create(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Read(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<T> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
