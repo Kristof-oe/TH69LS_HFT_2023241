2b037211b7332521a19ca7474149865dc10ecd11
Kristof-oe <kris200036@gmail.com>
2023-11-17 22:56:42 +0100

    Cat repo has been completed

--- TH69LS_HFT_2023241.Repository/Repository/CatRepository.cs
+++ TH69LS_HFT_2023241.Repository/Repository/CatRepository.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TH69LS_HFT_2023241.Models;
 
 namespace TH69LS_HFT_2023241.Repository.Repository
 {
-    internal class CatRepository
+    internal class CatRepository:Repository<Cat>, IRepository<Cat>
     {
+        public CatRepository(CatDbContext x) : base(x)
+        {
+        }
+
+        public override Cat Read(int ID)
+        {
+            return x.Cats.FirstOrDefault(x => x.ID == ID);
+        }
+
+        public override void Update(Cat item)
+        {
+            var old = Read(item.ID);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            x.SaveChanges();
+
+        }
     }
 }
