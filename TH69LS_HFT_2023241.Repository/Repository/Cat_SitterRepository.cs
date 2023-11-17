a4d9e7b7316a672a43d65763486b51a49bdefc6c
Kristof-oe <kris200036@gmail.com>
2023-11-17 22:43:32 +0100

    Cat sitter repository adjustment 1.0

--- TH69LS_HFT_2023241.Repository/Repository/Cat_SitterRepository.cs
+++ TH69LS_HFT_2023241.Repository/Repository/Cat_SitterRepository.cs
@@ -8,7 +8,20 @@ using TH69LS_HFT_2023241.Models;
 
 namespace TH69LS_HFT_2023241.Repository.Repository
 {
-    internal class Cat_SitterRepository : Repository<Cat_Sitter>
+    internal class Cat_SitterRepository : Repository<Cat_Sitter>, IRepository<Cat_Sitter>
     {
+        public Cat_SitterRepository(CatDbContext x) : base(x)
+        {
+        }
+
+        public override Cat_Sitter Read(int ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(Cat_Sitter item)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
