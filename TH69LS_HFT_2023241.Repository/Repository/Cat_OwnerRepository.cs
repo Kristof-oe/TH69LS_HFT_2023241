99dbe4f20201d17165c3b4fe49a5bd5a7850bdee
Kristof-oe <kris200036@gmail.com>
2023-11-17 23:09:09 +0100

    Cat owner repo has been completed

--- TH69LS_HFT_2023241.Repository/Repository/Cat_OwnerRepository.cs
+++ TH69LS_HFT_2023241.Repository/Repository/Cat_OwnerRepository.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TH69LS_HFT_2023241.Models;
 
 namespace TH69LS_HFT_2023241.Repository.Repository
 {
-    public class Cat_OwnerRepository
+    public class Cat_OwnerRepository:Repository<Cat_Owner>, IRepository<Cat_Owner>  
     {
+        public Cat_OwnerRepository(CatDbContext x) : base(x)
+        {
+        }
+
+        public override Cat_Owner Read(int ID)
+        {
+            return x.Cat_Owners.FirstOrDefault(x => x.ID == ID);
+        }
+
+        public override void Update(Cat_Owner item)
+        {
+            var old = Read(item.ID);
+            foreach (var prop in old.GetType().GetProperties())
+            {
+                prop.SetValue(old, prop.GetValue(item));
+            }
+            x.SaveChanges();
+
+        }
     }
 }
